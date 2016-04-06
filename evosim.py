Fixed payoff calculation for PGGiGame.
- Now instead of N, nc+nd is used
- Implemented animation of microsimulations
- Added axis to the plots

evosim.py
@@ -12,18 +12,21 @@ N = 5
 HUMAN_PLAYER = True
 PMATRIX = np.array([[0.8, 0.0],
                     [1.0, 0.2]])
-GENERATIONS = 1000
-THRESHOLD = 100
+GENERATIONS = 2000
+THRESHOLD = 0
 cost = 1.0
 r = 10.0
 r_min = 1.0
 r_max = 5.25
 r_step = 0.25
 nu = 1.0
-runs = 1
+runs = 100
 realizations = 1
-ncoop = 0.5
-ninsp = 0.5
+ncoop = 0.1
+ninsp = 0.8
+show_micro_simulations = False
+store_plots = False
+store_data = False
 
 # players = generate_players(N, [['TFTPlayer', 0.8], ['RandomPlayer', 0.0], ['ParlovPlayer', 0.2]])
 # game = NIPDGame(THRESHOLD, GENERATIONS, players)
@@ -32,7 +35,6 @@ ninsp = 0.5
 
 players = generate_players([['PureStrategyPlayer', 1.0]], nplayers=N, ncoop=ncoop, ninsp=ninsp)
 game = PGGiGame(players, threshold=THRESHOLD, generations=GENERATIONS, r=r, cost=cost, nu=nu)
-
 
 if __name__ == "__main__":
     level = logging.INFO
@@ -46,10 +48,18 @@ if __name__ == "__main__":
     insp_avg_real = np.empty([1, realizations])
     coop_avg = np.arange(r_min, r_max, r_step)
     insp_avg = np.arange(r_min, r_max, r_step)
+
+    if show_micro_simulations:
+        plt.ion()  # Note this correction
+        fig = plt.figure(1)
+        ax = plt.axes(xlim=(-1, THRESHOLD + GENERATIONS), ylim=(-0.1, 1.2))
+        r_text = ax.text(0.02, 0.95, '', transform=ax.transAxes)
+
     for idx, r_param in enumerate(r_params):
         # Update r value
         logger.info("Starting simulation with r = %f", r_param)
         game.r = r_param
+        r_start_time = time()
         for s in range(realizations):
             # Init graph
             for r in range(runs):
@@ -66,16 +76,19 @@ if __name__ == "__main__":
                 if game.__class__.__name__ == 'PGGiGame':
                     insp_avg_run[0][r] = np.mean(game.inspLevel)
 
-                # Save and Plot results
-                # plt.figure(1)
-                # plt.plot(x, game.coopLevel, label='Cooperation ratio', color='g')
-                # if game.__class__.__name__ == 'PGGiGame':
-                #     plt.plot(x, game.inspLevel, label='Inspectors ratio', color='b')
-                # plt.xlim(-1, THRESHOLD + GENERATIONS)
-                # plt.ylim(-0.1, 1.2)
-                # # plt.autoscale(True)
-                # plt.legend()
-                # plt.show()
+                if show_micro_simulations:
+                    plt.cla()
+                    # Save and Plot results
+                    r_text.set_text('r = %.3f' % r_param)
+                    ax.plot(x, game.coopLevel, label='Fraction of cooperators', color='g', lw=2)
+                    if game.__class__.__name__ == 'PGGiGame':
+                        ax.plot(x, game.inspLevel, label='Fraction of inspectors', color='b', lw=2)
+                    ax.set_xlabel("generations")
+                    ax.set_ylabel("Fraction of players")
+                    # plt.autoscale(True)
+                    ax.legend()
+                    plt.show()
+                    plt.pause(0.0001)  # Note this correction
 
                 # Init population
                 game.init_population(ncoop=ncoop, ninsp=ninsp)
@@ -83,18 +96,24 @@ if __name__ == "__main__":
             # Update the Avg.
             coop_avg_real[0][s] = np.mean(coop_avg_run)
             if game.__class__.__name__ == 'PGGiGame':
-                    insp_avg_real[0][s] = np.mean(insp_avg_run)
+                insp_avg_real[0][s] = np.mean(insp_avg_run)
 
         coop_avg[idx] = np.mean(coop_avg_real)
         insp_avg[idx] = np.mean(insp_avg_real)
+        r_interval = time() - r_start_time
+        logger.info("Simulation finished: elapsed time: %s seconds", r_interval)
 
+    if show_micro_simulations:
+        plt.ioff()
     # Save and Plot results
     plt.figure(2)
-    plt.plot(r_params, coop_avg, label='Cooperation ratio', color='g')
+    plt.plot(r_params, coop_avg, label='Fraction of cooperators', color='g')
     if game.__class__.__name__ == 'PGGiGame':
-        plt.plot(r_params, insp_avg, label='Inspectors ratio', color='b')
-    plt.xlim(0, r_max + 1)
+        plt.plot(r_params, insp_avg, label='Fraction of inspectors', color='b')
+    plt.xlim(r_min - 1, r_max + 1)
     plt.ylim(-0.1, 1.2)
+    plt.xlabel("r")
+    plt.ylabel("Fraction of players")
     # plt.autoscale(True)
     plt.legend()
     plt.show()

games/games.py
@@ -29,6 +29,7 @@ class AbstractGame:
             elif prob < (ncoop + ninsp):
                 action = 2
             player.init_action(action)
+            player.init_params()
 
     def run(self):
         avg_payoff = 0
@@ -99,7 +100,8 @@ class PGGGame(AbstractGame):
             # Evolve
             for player in self.population.values():
                 # Call evolve and update number of cooperators
-                self.nc += player.evolve(self.population[np.random.randint(0, self.N)])
+                dc, = player.evolve(self.population[np.random.randint(0, self.N)])
+                self.nc += dc
 
             if self.generations > self.threshold:
                 self.coopLevel[self.current_generation-self.threshold] = self.nc / self.N
@@ -113,6 +115,13 @@ class PGGiGame(PGGGame):
         self.ni = 0
         self.inspLevel = np.arange(0, generations, dtype=np.float64)
 
+    def calculate_payoff(self, action):
+        payoff = (self.nc*self.r*self.c)/(self.N - self.ni)
+        if action:
+            return payoff
+        else:
+            return payoff - self.c
+
     def get_inspected_player(self, pid):
         iid = np.random.randint(0, self.N)
         while iid == pid:
@@ -124,6 +133,19 @@ class PGGiGame(PGGGame):
     def inspect(self, inspector):
         pass
 
+    def init_game(self):
+        self.nc = self.N - 1
+        self.ni = 0
+        max_p = self.calculate_payoff(1)  # Max payoff
+        self.nc = 1
+        min_p = self.calculate_payoff(0)  # Min payoff
+        self.nc = 0
+        # Get number of cooperators
+        for player in self.population.values():
+            player.set_m_payoffs(max_p=max_p, min_p=min_p)
+            self.nc += 0 if player.action else 1
+            self.ni += 1 if (player.action == 2) else 0
+
     def run(self):
         for self.current_generation in range(self.generations + self.threshold):
             # Calculate payoffs
@@ -131,7 +153,6 @@ class PGGiGame(PGGGame):
                 if player.action != 2:
                     player.update_payoff(self.calculate_payoff(player.action))
                 else:
-                    self.ni += 1
                     iid = self.get_inspected_player(pid)
                     if iid == -1:
                         payoff = 0
@@ -144,12 +165,18 @@ class PGGiGame(PGGGame):
                     player.ngame += 1
 
             # Evolve
+            self.nc = 0
+            self.ni = 0
             for player in self.population.values():
                 # Call evolve and update number of cooperators
-                self.nc += player.evolve(self.population[np.random.randint(0, self.N)])
+                player.evolve(self.population[np.random.randint(0, self.N)])
+
+                if player.action == 0:
+                    self.nc += 1
+                elif player.action == 2:
+                    self.ni += 1
 
             logger.debug("[" + str(self.current_generation) + "] ncoop = " + str(self.nc) + " ninsp = " + str(self.ni))
             if self.generations > self.threshold:
                 self.coopLevel[self.current_generation-self.threshold] = self.nc / self.N
                 self.inspLevel[self.current_generation-self.threshold] = self.ni / self.N
-                self.ni = 0